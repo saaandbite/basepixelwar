f4adfe385346e8c660294575e43d368ca53117c5
Ahmad Dedad <projectdummy2025@gmail.com>
2026-01-10 20:46:47 +0700

    update server-bug

--- apps/web/app/game/hooks/usePvPGame.ts
+++ apps/web/app/game/hooks/usePvPGame.ts
@@ -21,7 +21,8 @@ type GameSocket = Socket<ServerToClientEvents, ClientToServerEvents>;
 const getServerUrl = () => {
     if (process.env.NEXT_PUBLIC_SOCKET_URL) return process.env.NEXT_PUBLIC_SOCKET_URL;
     if (typeof window !== 'undefined') {
-        return `http://${window.location.hostname}:3000`;
+        const protocol = window.location.protocol;
+        return `${protocol}//${window.location.hostname}:3000`;
     }
     return 'http://localhost:3000';
 };
